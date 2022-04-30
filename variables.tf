945ecc6d04be8a37eaf2bb34ff0aeeae53c4aab3
Agus Wibawa <aguscuk@gmail.com>
2022-04-30 22:23:36 +0700

    first commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+  description = "The project ID to host the network in"
+  default     = "learning01-348513"
+}
+
+variable "network_name" {
+  description = "The name of the VPC network being created"
+  default     = "example-vpc"
+}
